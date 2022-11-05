c734d1d2e68535652872ee61701a7e358bd203df
jagger-en <japman2400@gmail.com>
2022-11-05 20:51:43 +0200

    Allow update to order status

--- src/backend/resources/order.py
+++ src/backend/resources/order.py
@@ -10,6 +10,11 @@ from backend.utils.query_helpers import dump_results
 class OrderResource(Resource):
 
     def get(self):
+        form_data = dict(request.args)
+        if form_data.get('id'):
+            item = Order.query.get(form_data.get('id'))
+            return OrderSchema().dump(item), 200
+
         result = _query_all()
         return dump_results(result), 200
 
@@ -33,6 +38,27 @@ class OrderResource(Resource):
 
         return OrderSchema().dump(Order.query.get(new_entry.id)), 201
 
+
+    def put(self):
+        payload = request.json
+        if not isinstance(payload, dict):
+            try:
+                payload = json.loads(payload)
+            except Exception as e:
+                return f'Failed to parse payload: {e}', 400
+
+        order_id = payload['order_id']
+        new_status = payload['new_status']
+
+        my_order = Order.query.get(order_id)
+        if my_order is None:
+            return payload, 404
+
+        my_order.status = new_status
+        db.session.commit()
+
+        my_order = Order.query.get(order_id)
+        return OrderSchema().dump(my_order), 200
 
 @careful_query
 def _query_all():
--- tests/test_order.py
+++ tests/test_order.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 from base_classes import EndpointTestBaseClass
-
+import json
 
 class CheckEndpoints(EndpointTestBaseClass):
     def test_all_orders_shown(self):
@@ -20,6 +20,26 @@ class CheckEndpoints(EndpointTestBaseClass):
         self.assertEqual(response.json[4]['customer']['name'], 'Michael Steel')
         self.assertEqual(response.json[4]['city']['name'], 'City C')
         self.assertEqual(response.json[4]['product']['name'], 'my product 3')
+
+    def test_getting_order_by_id(self):
+        response = self.endpoint_client.get('/api/v1/order?id=1547')
+        self.assertEqual(response.json['id'], '1547')
+        self.assertEqual(response.json['product']['price'], 5400)
+
+    def test_order_update(self):
+        response = self.endpoint_client.get('/api/v1/order?id=1547')
+        my_order = response.json
+        self.assertEqual(my_order['id'], '1547')
+        self.assertEqual(my_order['status'], 'PENDING')
+
+        status_update_payload = {
+            'order_id': my_order['id'],
+            'new_status': 'APPROVED'
+        }
+        response = self.endpoint_client.put('/api/v1/order', json=status_update_payload)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['status'], 'APPROVED')
 
 
 if __name__ == '__main__':
